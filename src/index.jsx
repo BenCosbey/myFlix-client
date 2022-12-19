b82eafaf92b2f86198e20e7e7f3a4e510738fa81
BenCosbey <107746880+BenCosbey@users.noreply.github.com>
2022-12-18 22:58:32 -0800

    add
    3.9

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { MainView } from "./components/main-view/main-view";
+const cors = require('cors');
+let allowedOrigins = [...];
 
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
 import { Container, Row } from "react-bootstrap";
 import "./index.scss";
 
